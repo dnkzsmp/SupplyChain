37d715ddd97629c6b068fbfba6edb833f3658c89
Danil Kozlov <danilkozlov1003@gmail.com>
2020-09-29 18:45:48 +0700

    Add second constructor

--- src/main/java/com/curs/user/User.java
+++ src/main/java/com/curs/user/User.java
@@ -1,4 +1,4 @@
-package com.curs.database;
+package com.curs.user;
 
 public class User {
     private String name;
@@ -6,6 +6,16 @@ public class User {
     private int age;
     private String email;
     private String brand;
+
+    public User() {}
+
+    public User(String name, String surname, int age, String email, String brand) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+        this.brand = brand;
+    }
 
     public void setName(String name) {
         this.name = name;
