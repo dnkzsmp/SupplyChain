a847e8e89d663785f04c1a201c5f8cf5ec662413
Danil Kozlov <danilkozlov1003@gmail.com>
2020-10-11 17:49:57 +0700

    Change start point

--- src/main/java/com/curs/start/Main.java
+++ src/main/java/com/curs/start/Main.java
@@ -1,12 +1,9 @@
 package com.curs.start;
 
 import com.curs.gui.LoginFrame;
-import javax.swing.*;
-
 
 public class Main {
     public static void main(String[] args) {
-        JFrame frameLogin = new LoginFrame();
-        frameLogin.setVisible(true);
+        new LoginFrame().setVisible(true);
     }
 }
