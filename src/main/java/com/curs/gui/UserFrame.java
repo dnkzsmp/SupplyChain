22b143538ab747dcc76b50b7a712583453c9a0eb
Danil Kozlov <danilkozlov1003@gmail.com>
2020-10-11 17:49:42 +0700

    Add fucntion of resaving data

--- src/main/java/com/curs/gui/UserFrame.java
+++ src/main/java/com/curs/gui/UserFrame.java
@@ -1,11 +1,12 @@
 package com.curs.gui;
 
-import com.curs.database.DBaseAdmin;
+import com.curs.database.DBaseUA;
 import com.curs.user.User;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
@@ -30,7 +31,9 @@ public class UserFrame extends JFrame {
     private JPanel panelMain;
     private JButton buttonSave;
     private JButton buttonExit;
+    private JLabel labelAlert;
     private final ArrayList<User> people;
+    private final String [] arrOfTF = new String[5];
     private final DefaultListModel listPeopleModel;
 
     public UserFrame() {
@@ -38,8 +41,8 @@ public class UserFrame extends JFrame {
         setContentPane(panelMain);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
-        people = new ArrayList<User>();
-        final DBaseAdmin baseAdmin = new DBaseAdmin();
+        people = new ArrayList<>();
+        final DBaseUA baseAdmin = new DBaseUA();
         listPeopleModel = new DefaultListModel();
         listOfUsers.setModel(listPeopleModel);
         buttonSave.setEnabled(false);
@@ -48,32 +51,74 @@ public class UserFrame extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 int userNumber = listOfUsers.getSelectedIndex();
                 if (userNumber >= 0) {
-                    User u = people.get(userNumber);
-                    u.setName(textFieldName.getText());
-                    u.setSurname(textFieldSurname.getText());
-                    u.setAge(Integer.parseInt(textFieldAge.getText()));
-                    u.setEmail(textFieldEmail.getText());
-                    u.setBrand(textFieldBrand.getText());
-                    refreshUser();
+                    if (!textFieldAge.getText().isEmpty() &&
+                            !textFieldBrand.getText().isEmpty() &&
+                            !textFieldEmail.getText().isEmpty() &&
+                            !textFieldName.getText().isEmpty() &&
+                            !textFieldSurname.getText().isEmpty()) {
+                        if (isNumber(textFieldAge.getText())) {
+                            arrOfTF[0] = textFieldName.getText();
+                            arrOfTF[1] = textFieldSurname.getText();
+                            arrOfTF[2] = textFieldAge.getText();
+                            arrOfTF[3] = textFieldEmail.getText();
+                            arrOfTF[4] = textFieldBrand.getText();
+                            User u = people.get(userNumber);
+                            u.setName(textFieldName.getText());
+                            u.setSurname(textFieldSurname.getText());
+                            u.setAge(Integer.parseInt(textFieldAge.getText()));
+                            u.setEmail(textFieldEmail.getText());
+                            u.setBrand(textFieldBrand.getText());
+                            refreshUser();
+                            try {
+                                baseAdmin.changeDataInDB(u, arrOfTF);
+                            } catch (SQLException ex) {
+                                ex.printStackTrace();
+                            }
+                        } else {
+                            buttonSave.setEnabled(false);
+                            labelAlert.setText("Ошибка: введите число в поле 'Возраст'");
+                            labelAlert.setForeground(Color.red);
+                        }
+                    } else {
+                        buttonSave.setEnabled(false);
+                        labelAlert.setText("Ошибка: заполните все поля");
+                        labelAlert.setForeground(Color.red);
+                    }
                 }
             }
         });
+
         buttonSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                User user = new User(
-                        textFieldName.getText(),
-                        textFieldSurname.getText(),
-                        Integer.parseInt(textFieldAge.getText()),
-                        textFieldEmail.getText(),
-                        textFieldBrand.getText()
-                );
-                try {
-                    baseAdmin.setNewUserInDB(user);
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
+                User user;
+                if (!textFieldAge.getText().isEmpty() &&
+                        !textFieldBrand.getText().isEmpty() &&
+                        !textFieldEmail.getText().isEmpty() &&
+                        !textFieldName.getText().isEmpty() &&
+                        !textFieldSurname.getText().isEmpty()) {
+                    if (isNumber(textFieldAge.getText())) {
+                        user = new User(
+                                textFieldName.getText(),
+                                textFieldSurname.getText(),
+                                Integer.parseInt(textFieldAge.getText()),
+                                textFieldEmail.getText(),
+                                textFieldBrand.getText()
+                        );
+                        try {
+                            baseAdmin.setNewUserInDB(user);
+                        } catch (SQLException ex) {
+                            ex.printStackTrace();
+                        }
+                        addUser(user);
+                    } else {
+                        labelAlert.setText("Ошибка: ведите число в поле 'Возраст'");
+                        labelAlert.setForeground(Color.red);
+                    }
+                } else {
+                    labelAlert.setText("Ошибка: заполните все поля");
+                    labelAlert.setForeground(Color.red);
                 }
-                addUser(user);
             }
         });
         buttonExit.addActionListener(new ActionListener() {
@@ -88,20 +133,21 @@ public class UserFrame extends JFrame {
             public void valueChanged(ListSelectionEvent e) {
                 int userNumber = listOfUsers.getSelectedIndex();
                 if (userNumber >= 0) {
-                    User u = people.get(userNumber);
-                    textFieldName.setText(u.getName());
-                    textFieldSurname.setText(u.getSurname());
-                    textFieldAge.setText(Integer.toString(u.getAge()));
-                    textFieldEmail.setText(u.getEmail());
-                    textFieldBrand.setText(u.getBrand());
-                    buttonSave.setEnabled(true);
-                } else {
+                            User u = people.get(userNumber);
+                            textFieldName.setText(u.getName());
+                            textFieldSurname.setText(u.getSurname());
+                            textFieldAge.setText(Integer.toString(u.getAge()));
+                            textFieldEmail.setText(u.getEmail());
+                            textFieldBrand.setText(u.getBrand());
+                            buttonSave.setEnabled(true);
+                        } else {
                     buttonSave.setEnabled(false);
                 }
             }
         });
         setLocationRelativeTo(null);
     }
+
     private void refreshUser() {
         listPeopleModel.removeAllElements();
         System.out.println("Очистил лист пользователей");
@@ -114,5 +160,13 @@ public class UserFrame extends JFrame {
     private void addUser(User user) {
         people.add(user);
         refreshUser();
+    }
+
+    public boolean isNumber(String str) {
+        if (str == null || str.isEmpty()) return false;
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) return false;
+        }
+        return true;
     }
 }
